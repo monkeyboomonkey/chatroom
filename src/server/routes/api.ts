63d8bb10fba0d44452e297db48467a39b077a42f
Wenzhen Gong <kong20100902@hotmail.com>
2023-10-05 19:10:27 -0700

    finished server settings

--- src/server/routes/api.ts
+++ src/server/routes/api.ts
@@ -2,4 +2,6 @@ import express from 'express';
 import { getAllUsers } from '../controllers/chatRoomControllers.js'
 const router = express.Router();
 
-router.post('/createuser', getAllUsers, (req, res) => res.status(200).json(res.locals.allUsers));
+router.get('/getusers', getAllUsers, (req, res) => res.status(200).json(res.locals.allUsers));
+
+export default router;
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,20 @@
 import { Server } from "socket.io";
 import { createServer } from "http";
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import apiRouter from './routes/api.js';
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.resolve(__dirname, '../client')));
+app.use('/api', apiRouter);
+
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {origin: "*"}
