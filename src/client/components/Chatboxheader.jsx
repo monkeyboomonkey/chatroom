2dae6e5a3745bcd2c0ce9cf2612e75e28a5c4ac5
luchd <dacluc1999@gmail.com>
2023-10-10 09:51:00 +0700

    Added Chatboxheader component to show the current room

--- src/client/components/Chatboxheader.jsx
+++ src/client/components/Chatboxheader.jsx
@@ -5,7 +5,7 @@ function Chatboxheader(props) {
 
     return (
         <div>
-            {props.roomName ? <h3>Welcome to room #{props.roomName}</h3> : <h3>You're in the lobby. Pick your interested room to start chatting!</h3>}
+            {props.roomName !== undefined ? <h3>Welcome to room #{props.roomName}</h3> : <h3>You're in the lobby. Pick your interested room to start chatting!</h3>}
         </div>
     );
 }
