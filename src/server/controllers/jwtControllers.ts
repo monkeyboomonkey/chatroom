c721f87bc8d7f82b1278c0d29cf7785fd9f31253
Tristan Williams <tristanscottwilliams@gmail.com>
2023-10-10 19:27:05 -0500

    verify almost working, fixed some others server functionality

--- src/server/controllers/jwtControllers.ts
+++ src/server/controllers/jwtControllers.ts
@@ -6,10 +6,16 @@ dotenv.config();
 
 export function createJWT(req: Request, res: Response, next: NextFunction): void {
   const token = jwt.sign({ userid: res.locals.user.userid }, String(process.env.JWT_SECRET), {expiresIn: 60});
+  res.locals.token = token;
   res.cookie("jwt", token, {httpOnly: true, secure: true})
   return next();
 }
 
 export function verifyJWT(req: Request, res: Response, next: NextFunction): void {
-  
+  try { 
+    jwt.verify(res.locals.token, String(process.env.JWT_SECRET));
+    return next();
+  } catch { 
+    return next('error verifying your jwt') 
+  };
 }
