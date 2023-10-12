65197f846b79ffd9f31f940c236ded614258076d
Tristan Williams <tristanscottwilliams@gmail.com>
2023-10-11 21:15:36 -0500

    code should be all up to date now

--- src/server/routes/api.ts
+++ src/server/routes/api.ts
@@ -1,11 +1,8 @@
 import express, { Request, Response } from 'express';
 import { getAllUsers, registerUser, userLogIn, deleteUser, updateUser } from '../controllers/userControllers.js'
-<<<<<<< HEAD
-=======
 import { getAllChatrooms, addChatroom } from '../controllers/chatroomControllers.js'
 import { addChatlog, getAllChatlogs } from '../controllers/chatlogControllers.js'
 
->>>>>>> WG/middleware-refactor-with-jwt2
 import { createJWT, verifyJWT } from '../controllers/jwtControllers.js'
 
 export const router = express.Router();
@@ -14,12 +11,6 @@ export const router = express.Router();
 // intend to add createJWT(renew JWT) after userLogIn, verifyJWT before and createJWT(renewJWT) after updateUser in '/updateuser' because it is a user operation
 // intend to add verifyJWT before deleteUser because it is a user operation
 router.get('/getallusers', getAllUsers, (req: Request, res: Response):void => {res.status(200).json(res.locals.allUsers)});
-<<<<<<< HEAD
-router.post('/registeruser', registerUser, /*createJWT, verifyJWT,*/ (req: Request, res: Response):void => {res.status(200).json('user registered')});
-router.post('/userlogin', userLogIn, createJWT, verifyJWT, (req: Request, res: Response):void => {res.status(200).json(res.locals.user)});
-router.delete('/deleteuser', deleteUser, (req: Request, res: Response):void => {res.status(200).json('user deleted')});
-router.patch('/updateuser', userLogIn, updateUser, (req: Request, res: Response):void => {res.status(200).json(res.locals.user)});
-=======
 router.post('/registeruser', registerUser, (req: Request, res: Response):void => {res.status(200).json('user registered')});
 router.post('/userlogin', userLogIn, createJWT, (req: Request, res: Response):void => {res.status(200).json(res.locals.user);});
 router.delete('/deleteuser', verifyJWT, deleteUser, (req: Request, res: Response):void => {res.status(200).json('user deleted')});
@@ -34,4 +25,3 @@ router.post('/addchatroom', verifyJWT, addChatroom, createJWT, (req: Request, re
 // intend to add verifyJWT before and createJWT(renew JWT) after addChatlog and getAllChatlogs because they are user operations
 router.post('/addchatlog', addChatlog, (req: Request, res: Response):void => {res.status(200).json(res.locals.chatlog)});
 router.get('/getallchatlogs', getAllChatlogs, (req: Request, res: Response):void => {res.status(200).json(res.locals.chatlogs_in_chatroom)});
->>>>>>> WG/middleware-refactor-with-jwt2
